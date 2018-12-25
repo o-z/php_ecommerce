1e9a7e33cc02e2a952c994e30fa8d04200448a6b
Oguz Zeyveli <oguzzeyveli@gmail.com>
2018-12-25 09:24:14 +0300

    Case 9 Kategori bazlı urun listeleme - Case 13 Urun detayları - Case 23 Kategorileme

--- resources/functions.php
+++ resources/functions.php
@@ -18,14 +18,14 @@ function confirm($result){
 }
 function escape_string($string){
   global $connection;
-  return msqli_real_escape_string($connection, $string);
+  return mysqli_real_escape_string($connection, $string);
 }
 
 function fetch_array($result){
    return mysqli_fetch_array($result);
 }
 
-
+/*****front end fonctions*****/
 
 //get products
 
@@ -39,13 +39,56 @@ $product = <<<DELIMETER
 
 <div class="col-sm-4 col-lg-4 col-md-4">
     <div class="thumbnail">
-        <a href="item.php?id={$row['product_id']}"> <img src="{$row['product_image']}" alt=""></a>
+        <a href="item.php?id={$row['product_id']}"> <img src="{$row['product_image']}" alt="" style="width:320px;height:150px;"></a>
         <div class="caption">
             <h4 class="pull-right">&#36;{$row['product_price']}</h4>
-            <h4><a href="product.html">{$row['product_title']}</a>
+            <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
             </h4>
-            <p>{$row['product_description']}</p>
-            <a class="btn btn-primary"  href="item.php?id={$row['product_id']}">Add to Cart</a>
+            <p>{$row['short_desc']}</p>
+            <a class="btn btn-primary"  href="item.php?id={$row['product_id']}">Buy Now</a>
+            <a class="btn btn-primary"  href="item.php?id={$row['product_id']}">More Info</a>
+        </div>
+
+    </div>
+</div>
+
+DELIMETER;
+echo $product;
+}
+
+}
+function get_categories(){
+  $query = query("SELECT * FROM categories");
+  confirm($query);
+  while ($row = fetch_array($query)) {
+$category_links = <<<DELIMETER
+
+     <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+DELIMETER;
+echo $category_links;
+  }
+
+
+}
+
+function get_products_in_cat_page(){
+
+$query= query(" SELECT * FROM products WHERE product_category_id= " .escape_string($_GET['id']) . "");
+confirm($query);
+
+while ($row = fetch_array($query)) {
+$product = <<<DELIMETER
+
+<div class="col-md-3 col-sm-6 hero-feature">
+    <div class="thumbnail">
+        <a href="item.php?id={$row['product_id']}"> <img src="{$row['product_image']}" alt="" style="width:320px;height:150px;"></a>
+        <div class="caption">
+            <h4 class="pull-right">&#36;{$row['product_price']}</h4>
+            <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+            </h4>
+            <p>{$row['short_desc']}</p>
+            <a class="btn btn-primary"  href="item.php?id={$row['product_id']}">Buy Now</a>
+            <a class="btn btn-primary"  href="item.php?id={$row['product_id']}">More Info</a>
         </div>
 
     </div>
@@ -57,7 +100,7 @@ echo $product;
 
 }
 
-
+/*****Back end fonctions*****/
 function login_user(){
 
 if(isset($_POST['submit'])){
